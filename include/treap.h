53695254f82d674f70a0badb2803fbab3858af99
Sujai Ram Prasath C <sujairamprasath@yahoo.in>
2018-10-08 20:00:38 +0530

    Updated iterator class and did minor beautification

--- include/treap.h
+++ include/treap.h
@@ -1,10 +1,11 @@
 #ifndef TREAP_H
 #define TREAP_H
 
-#include<utility>
+#include <utility>
 #include <functional>
 
-template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare=std::less<KeyType>, class PriorityCompare = std::less<PriorityType>>
+
+template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare = std::less<KeyType>, class PriorityCompare = std::less<PriorityType>>
 class Treap
 {
 private:
@@ -12,28 +13,45 @@ private:
     {
         KeyType Key;
         ValueType Value;
-        unsigned priority;
+        PriorityType priority;
         Node* left_child;
         Node* right_child;
         Node* parent;
     };
-    unsigned size;
+
+    unsigned int size;
     Node* root;
 
-    //Node* transplant(Node*, Node*);
-
-
 public:
-
+    /* Iterator class defnition */
     class iterator {
     private:
-        Node* pointer;
-
+        Node *pointer;
     public:
-        iterator (arguments);
-        ~iterator ();
+        iterator();
+        iterator(Node*);
+
+        const ValueType& operator*() const;
+        iterator& operator++();
+        iterator& operator--();
+        iterator& operator++(int);
+        iterator& operator--(int);
+
+        friend bool operator==(const BinarySearchTree<KeyType, ValueType>::iterator &iterator_1, const BinarySearchTree<KeyType, ValueType>::iterator &iterator_2) {
+            if (iterator_1.pointer == iterator_2.pointer) {
+                return true;
+            }
+            return false;
+        }
+        friend bool operator!=(const BinarySearchTree<KeyType, ValueType>::iterator &iterator_1, const BinarySearchTree<KeyType, ValueType>::iterator &iterator_2) {
+            if (iterator_1.pointer != iterator_2.pointer) {
+                return true;
+            }
+            return false;
+        }
     };
 
+    /* Constructors & Destructors */
     Treap();
     Treap(const Treap&);
     Treap(Treap&&);
@@ -42,21 +60,21 @@ public:
 
     Treap& operator=(Treap);
 
+    /* Capacity */
     bool isEmpty() const;
     unsigned getSize() const;
 
+    /* Iterators */
     iterator begin() const;
     iterator end() const;
 
+    /* Modifiers */
     void merge(Treap&&);
     std::pair<Treap, Treap> split();
-
     void insert(const KeyType&, ValueType value);
     void erase(const KeyType&);
     ValueType& search(const KeyType&) const;
-
-
 };
 
 
-#endif
+#endif /* TREAP_H */
