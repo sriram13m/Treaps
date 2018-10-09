6dccb55c60f51e8471a3b396d419c609e0632601
Sujai Ram Prasath C <sujairamprasath@yahoo.in>
2018-10-09 18:24:45 +0530

    Added a Makefile, initial implementation of Treaps.cc along with a test file

---
+++ src/Treap.cc
@@ -0,0 +1,35 @@
+template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare, class PriorityCompare>
+Treap<KeyType, ValueType, PriorityType>::Treap() : root(nullptr), size(0)
+{}
+
+
+
+template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare, class PriorityCompare>
+Treap<KeyType, ValueType, PriorityType>::Treap(const Treap&)
+{
+    // TODO : Implement this
+}
+
+
+
+template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare, class PriorityCompare>
+Treap<KeyType, ValueType, PriorityType>::Treap(Treap&&)
+{
+    // TODO : Implement this
+}
+
+
+
+template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare, class PriorityCompare>
+Treap<KeyType, ValueType, PriorityType>::~Treap()
+{
+    // TODO : Implement this
+}
+
+
+
+template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare, class PriorityCompare>
+Treap<KeyType, ValueType, PriorityType>::Treap& operator=(Treap)
+{
+    // TODO : Implement this
+}
---
+++ test/Treap.cpp
@@ -0,0 +1,8 @@
+#include "../include/Treap.h"
+
+int main()
+{
+    Treap<int, int, int> t;
+
+    return 0;
+}
