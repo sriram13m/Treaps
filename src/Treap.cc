fbbf7c1f7c39cd3cbc42a53aeaf0291022cc0fa1
Sriram <sriram13k@gmail.com>
2018-10-11 16:40:34 +0530

    Updated Helper Functions

--- src/Treap.cc
+++ src/Treap.cc
@@ -1,3 +1,4 @@
+
 template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare, class PriorityCompare>
 Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>::Treap() : root(nullptr), size(0)
 {}
@@ -7,7 +8,7 @@ Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>::Treap() : 
 template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare, class PriorityCompare>
 Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>::Treap(const Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare> &other)
 {
-    // TODO : Implement this
+
 }
 
 
@@ -15,7 +16,10 @@ Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>::Treap(cons
 template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare, class PriorityCompare>
 Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>::Treap(Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare> &&other)
 {
-    // TODO : Implement this
+    root = other.root;
+    size = other.size;
+    other.root = nullptr;
+    other.size = 0;
 }
 
 
@@ -27,9 +31,25 @@ Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>::~Treap()
 }
 
 
-
 template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare, class PriorityCompare>
 Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>& Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>::operator=(Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare> other)
 {
     // TODO : Implement this
+}
+
+template<typename KeyType, typename ValueType , typename PriorityType, class KeyCompare, class PriorityCompare>
+unsigned Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>::getSize() const
+{
+    return size;
+}
+
+template<typename KeyType, typename ValueType, typename PriorityType, class KeyCompare,class PriorityCompare>
+bool Treap<KeyType, ValueType, PriorityType, KeyCompare, PriorityCompare>::isEmpty() const
+{
+    if(size == 0)
+    {
+        return true;
+    }
+
+    return false;
 }
